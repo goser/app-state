89f78953c46d3b3d9228a8e2c5fff35b972f2967
Michael Placke <mplacke@magix.net>
2024-05-23 13:52:07 +0200

    Add main and types root

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+export * from './hooks/useDispatch';
+export * from './hooks/useSelector';
+export * from './hooks/useStore';
+export * from './hooks/StoreContext';
+
+export * from './reducer/combineReducers';
+export * from './reducer/Reducer';
+
+export * from './store/AsyncAction';
+export * from './store/ExtractAction';
+export * from './store/Loader';
+export * from './store/Store';
+export * from './store/StoreSubscriber';
+export * from './store/TypedAction';
